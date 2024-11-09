ad63f56951793c2b864e8b5c2b3b47f56c13dfe5
chiragsinghal96 <chiragsinghal2406@gmail.com>
2024-11-09 22:26:33 +0530

    errorfix

--- src/Config/gemini.js
+++ src/Config/gemini.js
@@ -4,8 +4,8 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai" 
 
-const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY || process.env.VITE_GEMINI_API_KEY);
+
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
